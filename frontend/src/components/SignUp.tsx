b0e7bbc09d763962455f7c2a507186e23d682e63
Kaitlyn Armstrong <72315278+KaitMArms@users.noreply.github.com>
2025-10-09 09:31:53 -0400

    Added templates for sign up page

---
+++ frontend/src/components/SignUp.tsx
@@ -0,0 +1,24 @@
+function SignUp()
+{
+    function doLogin(event:any) : void
+    {
+        event.preventDefault();
+        alert('doIt()');
+    }
+
+    // put function for sign up here that calls login in after signup okayed by server
+
+    return(
+        <div id="signUpDiv">
+        <span id="inner-title">PLEASE SIGN UP</span><br />
+        <input type="text" id="firstName" placeholder="First Name"/><br />
+        <input type="text" id="lastName" placeholder="Last Name"/><br />
+        <input type="text" id="loginName" placeholder="Username" /><br />
+        <input type="password" id="loginPassword" placeholder="Password" /><br />
+        <input type="submit" id="loginButton" className="buttons" value = "Do It"
+        onClick={doLogin} />
+        <span id="loginResult"></span>
+        </div>
+    );
+};
+export default SignUp;
---
+++ frontend/src/pages/SignUpPage.tsx
@@ -0,0 +1,12 @@
+import PageTitle from '../components/PageTitle.tsx';
+import SignUp from '../components/Login.tsx';
+const SignUpPage = () =>
+{
+    return(
+        <div>
+            <PageTitle />
+            <SignUp />
+        </div>
+    );
+};
+export default SignUpPage;
