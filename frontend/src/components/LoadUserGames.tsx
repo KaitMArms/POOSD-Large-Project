e61523ffddb42404a03cf46dd915e3c49f54e60e
Kaitlyn Armstrong <72315278+KaitMArms@users.noreply.github.com>
2025-10-14 10:52:24 -0400

    Set up template for loading the user's game list

--- frontend/src/components/LoadUserGames.tsx
+++ frontend/src/components/LoadUserGames.tsx
@@ -1,2 +1,23 @@
 // Loads page set up for user's personal game library and associated progress
 // Sections: Completed, In Progress, Dropped, To Play
+function LoadUserGames()
+{
+    return(
+        //code inside container, display game in their proper catagories
+        <div id="GameContainer">
+            <div id="completedGame">
+                
+            </div>
+            <div id="inProgGame">
+                
+            </div>
+            <div id="droppedGame">
+                
+            </div>
+            <div id="toPlayGame">
+                
+            </div>
+        </div>
+    );
+};
+export default LoadUserGames;
