9208a30e1ed2adbfb2a014f940f260f25d76818a
Gage Lappin <gagelappin119@gmail.com>
2025-10-27 12:55:30 -0400

    Added comments

--- backend/models/Games.js
+++ backend/models/Games.js
@@ -2,16 +2,22 @@ const mongoose = require('mongoose');
 const { gameConnection } = require('../db');
 
 
+// Mongoose schema to store the Game schema
 const gameSchema = new mongoose.Schema({
+    // IGDB game reference id
     id: { type: Number, required: true}, 
     name: { type: String, required: true },
+    // url-tag
     slug: { type: String },
     summary: { type: String },
     first_release_date: { type: Number }, 
     genres: [ {type: Number} ], 
+    // https://api.igdb.com/v4/covers
+    // cover stores the id for the url
     cover: { type: Number },
     age_ratings: [{type : Number}],
     franchise: {type: Number},
+    // Stores what platform the game is available on
     platforms: [ {type: Number}],
     rating: {type : Number, default: 0},
     themes: [ {type: Number}],
@@ -25,6 +31,8 @@ const gameSchema = new mongoose.Schema({
     timestamps: true 
 });
 
+// Requires the IGDB index to be unique
 gameSchema.index({id: 1}, {unique: true});
 
+// Creates the Game collection
 module.exports = gameConnection.models.game || gameConnection.model('Game', gameSchema);
