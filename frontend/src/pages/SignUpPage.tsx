6fe9ef2f001f21b4c918037a06f3ec6f683119e3
Kaitlyn Armstrong <72315278+KaitMArms@users.noreply.github.com>
2025-10-28 19:13:43 -0400

    Removed page titles from login and sign up

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,11 +1,9 @@
-import PageTitle from '../components/PageTitle.tsx';
 import SignUp from '../components/SignUp.tsx';
 import "./SignUp.css"
 const SignUpPage = () =>
 {
     return(
         <div>
-            <PageTitle />
             <SignUp />
         </div>
     );
