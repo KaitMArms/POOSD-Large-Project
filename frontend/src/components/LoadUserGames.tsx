f7f3d3d8f41c284662f93ebddbf309f2a000ec3c
Kaitlyn Armstrong <72315278+KaitMArms@users.noreply.github.com>
2025-10-29 20:27:28 -0400

    Most Main Pages Now Have HTML, CSS started
    everything besides user profile go and css files started to be set up

--- frontend/src/components/LoadUserGames.tsx
+++ frontend/src/components/LoadUserGames.tsx
@@ -4,18 +4,29 @@ function LoadUserGames()
 {
     return(
         //code inside container, display game in their proper catagories
-        <div id="GameContainer">
-            <div id="completedGame">
-                
-            </div>
-            <div id="inProgGame">
-                
-            </div>
-            <div id="droppedGame">
-                
-            </div>
-            <div id="toPlayGame">
-                
+        <div id="page-container">
+            <span id="page-title">Your Games List</span><br />
+            <div id="game-container">
+                <div id="game-column">
+                    <span id="column-title">Completed</span>
+                    <span id="completed-games"></span>
+                </div>
+                <div id="game-column">
+                    <span id="column-title">In Progress</span>
+                    <span id="in-prog-games"></span>
+                </div>
+                <div id="game-column">
+                    <span id="column-title">Paused</span>
+                    <span id="paused-games"></span>
+                </div>
+                <div id="game-column">
+                    <span id="column-title">Dropped</span>
+                    <span id="dropped-games"></span>
+                </div>
+                <div id="game-column">
+                    <span id="column-title">To Be Played</span>
+                    <span id="to-play-games"></span>
+                </div>
             </div>
         </div>
     );
