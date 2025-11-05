53afd73e3281334cb7f26aa257856d9cb4762cae
Gage Lappin <gagelappin119@gmail.com>
2025-11-05 13:26:54 -0500

    Updated importGames to include rating / rating_count

--- backend/scripts/import_games.js
+++ backend/scripts/import_games.js
@@ -35,7 +35,7 @@ async function importGames() {
             
             // Request to twitch's servers, requests information about limit games with an increment of offset
             const igdbResponse = await axios.post('https://api.igdb.com/v4/games',
-                `fields name,slug,summary,first_release_date,genres,cover,age_ratings,franchise,platforms,game_modes,keywords,language_supports,themes,game_type,player_perspectives; limit ${limit}; offset ${offset}; where platforms != null;`,
+                `fields name,slug,summary,first_release_date,genres,cover,age_ratings,franchise,platforms,game_modes,keywords,language_supports,themes,game_type,player_perspectives, rating, rating_count; limit ${limit}; offset ${offset}; where platforms != null;`,
                 {
                     headers: {
                         'Client-ID': process.env.TWITCH_CLIENT_ID,
