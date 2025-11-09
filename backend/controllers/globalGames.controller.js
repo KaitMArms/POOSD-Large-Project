3dfda7e2e3b16d0a32b3287c01fb52d1fcaaaf71
Martex6 <maraguilar1009@gmail.com>
2025-11-08 22:57:19 -0500

    Implemented browseRecommended - View recommended globalGames It won't be necessary to add pages since the algorithm returns a set number of recommended games. The actual functions are Implemented on a folder called services within main and the databse branch. Need to merge with main any time soon.
    Aside from that, haven't tested.

--- backend/controllers/globalGames.controller.js
+++ backend/controllers/globalGames.controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/Users');
 const Game = require('../models/Games');
+const recommend = require('../services/recommend')
 
 function escapeRegex(s) {
   return s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
@@ -35,6 +36,28 @@ exports.browseGames = async (req, res) => {
     return res.status(500).json({ message: 'Server error.' });
   }
 };
+
+exports.browseRecommended = async (req,res) => {
+
+  try{
+
+    //const page = Math.max(parseInt(req.query.page || '1', 10), 1);
+    //const limitReq = Math.max(parseInt(req.query.limit || '50', 10), 1);
+    //const limit = Math.min(limitReq, 50); // cap at 50 per page
+
+    //Function to get array of recommended games IDs.
+    const gamesIDs = recommend.recommendedGames;
+    
+    // Get games by IDs
+    const games = await Game.find({ _id: { $in: gamesIDs}});
+    return res.json({games});
+
+  }catch(error) {
+    console.error('browseRecommended error:', err);
+    return res.status(500).json({ message: 'Server error.' });
+  }
+
+}
 
 exports.searchGames = async (req, res) => {
   try {
@@ -107,7 +130,13 @@ exports.searchGames = async (req, res) => {
 };
 
 exports.recommendedGames = async(req, res) => {
-    //via LLM, recommend games based on user's preferences
+
+    // Get usergames IDs
+    let curUser = req.user.userID;
+    let GamesIDs = curUser.userGames;
+    // Call functions for recommending games
+    return recommend.getRecommendations(user_profile.buildUserProfileVector, GamesIDs).json;
+
 };
 
 exports.addUserGame = async (req, res) => {
