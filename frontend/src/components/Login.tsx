4d25c25460a8ef4205e879f0ea060828e9e368ab
Kaitlyn Armstrong <72315278+KaitMArms@users.noreply.github.com>
2025-11-09 14:59:00 -0500

    Added link to sign up

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -9,12 +9,16 @@ function Login()
     return(
         <div id="page-container">
             <img src="Mascot.png" alt="Controllie - PlayedIt's Mascot, he's a living breathing controller"></img>
+            <br></br>
             <div id="login-div">
                 <span id="inner-title">Welcome Back to PlayedIt!</span><br />
                 <input type="text" id="loginName" placeholder="Username" /><br />
                 <input type="password" id="loginPassword" placeholder="Password" /><br />
-                <input type="submit" id="loginButton" className="buttons" value = "Do It" onClick={doLogin} />
+                <input type="submit" id="loginButton" className="buttons" value = "Log In" onClick={doLogin} />
                 <span id="login-result"></span>
+                <span id="inner-title">New to PlayedIt? Make an account.</span><br />
+                <a href="../pages/SignUpPage.tsx">Sign Up</a>
+                
             </div>
         </div>
     );
