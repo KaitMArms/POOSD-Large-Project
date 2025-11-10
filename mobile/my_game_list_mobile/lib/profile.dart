f158b6a99022ff431360b00b077154cdeb368c9e
erkins-zea <erkins.34758@gmail.com>
2025-11-09 22:35:27 -0500

    bottom icon page added, profile functionality being implemented

--- mobile/my_game_list_mobile/lib/profile.dart
+++ mobile/my_game_list_mobile/lib/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 class Profile extends StatefulWidget {
   const Profile({super.key});
 
@@ -9,7 +10,49 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Text("Profile"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {}, 
+            icon: Icon(MdiIcons.accountSettings) 
+          )
+        ],
+      ),
+      //default Scaffold retains base colors of light mode = light blue, dark mode = dark blue
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 3,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.accountOutline),
+            activeIcon: Icon(MdiIcons.account),
+            label: "Explore"
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.chartPieOutline),
+            activeIcon: Icon(MdiIcons.chartPie),
+            label: "Your Info",
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.lockOutline),
+            activeIcon: Icon(MdiIcons.lock),
+            label: "For Later"
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.accountOutline),
+            activeIcon: Icon(MdiIcons.account),
+            label: "Profile"
+          ),
+        ]
+      ),
+    );
   }
 }
