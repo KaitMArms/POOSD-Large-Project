aef285ede260e47365d128876ec854abe3fcad96
Kaitlyn Armstrong <72315278+KaitMArms@users.noreply.github.com>
2025-11-10 16:26:38 -0500

    Attempt to fix day night mode

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -2,8 +2,10 @@
 import PageTitle from '../components/PageTitle.tsx';
 import LoadUser from '../components/LoadUser.tsx';
 import LoadDevUser from '../components/LoadDevUser.tsx';
-import "../pages/DevUserProfile.css"
-import "../pages/UserProfile.css"
+import "../pages/DevUserProfile.css";
+import "../pages/UserProfile.css";
+import "../index.css";
+
 const UserProfilePage = () =>
 {
     const devFlag = false; // figure out dynamic solution temp placeholder
