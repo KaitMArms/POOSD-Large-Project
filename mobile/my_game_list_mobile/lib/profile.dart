337528d4afe2a0793906adbc3caf3009cf36c75a
erkins-zea <erkins.34758@gmail.com>
2025-11-10 14:51:31 -0500

    more profile page modifications
    further customization to come, backbone done

--- mobile/my_game_list_mobile/lib/profile.dart
+++ mobile/my_game_list_mobile/lib/profile.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -24,9 +26,10 @@ class _ProfileState extends State<Profile> {
         ],
       ),
       body: ListView(
-        //padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+        padding: EdgeInsets.symmetric(horizontal: 20),
         children:  [
           Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50,
@@ -43,10 +46,110 @@ class _ProfileState extends State<Profile> {
               Text(
                 "Connect Number of Games Committed Here (Possibly if Not for Your Data Part, either that or email API too)"
               ),
+              const SizedBox(height: 25),
+              Row(
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.only(right: 5),
+                    child: Text(
+                      "Complete Your Profile",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text("(1/5)"),
+                ],
+              ),
+
+              SizedBox(height: 10),
+              Row(
+                children: List.generate(5, (index) {
+                  return Expanded(
+                    child: Container(
+                      height: 7,
+                      width: 10,
+                      margin: EdgeInsets.only(right: index == 4 ? 0 : 6),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: index == 0 ? Colors.blue : Colors.black38,
+                      ),
+                    ),
+                  );
+                },),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: 160,
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    final card = profileCompletionCards[index];
+                    return SizedBox(
+                      width: 160,
+                      child: Card(
+                        shadowColor: Colors.black12,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Column(
+                            children: [
+                              Icon(
+                                card.icon,
+                                size: 30,
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                  card.title,
+                                  textAlign: TextAlign.center,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                              ),
+                              Spacer(),
+                              ElevatedButton(onPressed: () {
+                              }, 
+                              style: ElevatedButton.styleFrom(
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )
+                              ),
+                              child: Text(card.buttonText)),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  }, 
+                  separatorBuilder: (context, index) => Padding(padding: EdgeInsets.only(right: 5)), 
+                  itemCount: profileCompletionCards.length,
+                ),
+              ),
+              SizedBox(height: 35),
+              ...List.generate(
+                customListTiles.length, 
+              (index) {
+                final tile = customListTiles[index];
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 5.0),
+                  child: Card(
+                    elevation: 4,
+                    shadowColor: Colors.black12,
+                    child: ListTile(
+                      title: Text(tile.title),
+                      leading: Icon(tile.icon),
+                      trailing: Icon(Icons.chevron_right),
+                    ),
+                  ),
+                );
+              }
+              ),
             ],
-          )
+          ),
         ],
       ),
+
       //default Scaffold retains base colors of light mode = light blue, dark mode = dark blue
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 3,
@@ -80,3 +183,63 @@ class _ProfileState extends State<Profile> {
     );
   }
 }
+class ProfileCompletionCard {
+  final String title;
+  final String buttonText;
+  final IconData icon;
+
+  ProfileCompletionCard({
+    required this.title,
+    required this.buttonText,
+    required this.icon,
+  });
+}
+
+List<ProfileCompletionCard> profileCompletionCards = [
+  ProfileCompletionCard(
+    title: "Set Your Profile Details",
+    buttonText: "Continue",
+    icon: CupertinoIcons.person_circle,
+  ),
+  ProfileCompletionCard(
+    title: "Upload Your Resume",
+    buttonText: "Upload",
+    icon: CupertinoIcons.doc,
+  ),
+  ProfileCompletionCard(
+    title: "Add Your Skills",
+    buttonText: "Add",
+    icon: CupertinoIcons.square_list,
+  ),
+];
+
+class CustomListTiles {
+  final IconData icon;
+  final String title;
+  CustomListTiles({
+    required this.icon,
+    required this.title,
+  });
+}
+
+List<CustomListTiles> customListTiles = [
+  CustomListTiles(
+    icon: Icons.insights, 
+    title: "Activity",
+  ),
+
+  CustomListTiles(
+    icon: Icons.location_on_outlined, 
+    title: "Location"
+  ),
+
+  CustomListTiles(
+    icon: CupertinoIcons.bell, 
+    title: "Notifications"
+  ),
+
+  CustomListTiles(
+    icon: CupertinoIcons.arrow_right_arrow_left, 
+    title: "Logout"
+  ),
+];
