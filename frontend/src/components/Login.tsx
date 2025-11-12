79411ce459434e2f63dc962a7647e6119799d35e
Kaitlyn Armstrong <72315278+KaitMArms@users.noreply.github.com>
2025-11-12 09:49:35 -0500

    Login in navigation patch
    It was bring it to dashboard and not profile for some god forsaken reason so this should fix that

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,30 +1,36 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login()
 {
     const [showPassword, setShowPassword] = useState(false);
+    const navigate = useNavigate();
 
     async function doLogin(event: any): Promise<void> {
-        event.preventDefault();
-        const loginName = (document.getElementById("loginName") as HTMLInputElement).value;
-        const loginPassword = (document.getElementById("loginPassword") as HTMLInputElement).value;
+    event.preventDefault();
+    const loginName = (document.getElementById("loginName") as HTMLInputElement).value;
+    const loginPassword = (document.getElementById("loginPassword") as HTMLInputElement).value;
 
-        try {
-        const response = await fetch('http://localhost:8080/api/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ login: loginName, password: loginPassword })
+    try {
+        const response = await fetch("http://localhost:8080/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ login: loginName, password: loginPassword }),
         });
 
         const data = await response.json();
-        if (response.ok) console.log('Login successful:', data);
-        else console.error('Login failed:', data.message);
 
-        } 
-        catch (error) {
-            console.error('An error occurred:', error);
+        if (response.ok) {
+        console.log("Login successful:", data);
+        localStorage.setItem("token", data.token);
+        navigate("/profile");
+        } else {
+        console.error("Login failed:", data.message);
+        document.getElementById("login-result")!.textContent = "Invalid username or password.";
         }
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
     }
 
     return(
