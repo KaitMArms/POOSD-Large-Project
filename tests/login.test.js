5e06d2ee876f18634bacd9c3d4f56fbf5feaf32d
Martex6 <maraguilar1009@gmail.com>
2025-11-15 00:11:32 -0500

    Finished implementing test for login

--- tests/login.test.js
+++ tests/login.test.js
@@ -10,6 +10,33 @@ const bcrypt = ('bycryptjs');
 jest.mock("../backend/models/Users");
 jest.mock("bycryptjs");
 
-test("POST ../backend/auth/login", () => {
+// Test for login with standard account
+describe("POST ../backend/auth/login", () => {
+
+    // Test login
+    Test("Returns token, id, name, email, lastname, role, emailverified, userID, otpLastSentAt", async() => {
+        //Get user
+        User.findOne.mockResolvedValue({
+            _id: "6915fab55fa7852213445b7c",
+            email: "juela575@gmail.com",
+            password: "COP4331"
     
+        });
+
+        // Verify
+        bcrypt.localeCompare.mockResolvedValue(true);
+
+        const response = await request(app).post("auth/login").send({
+            email: "juela575@gmail.com",
+            password: "COP4331"
+        });
+
+        expect(response.status).toBe();
+        expect(response.body).toHaveProperty("token", "user");
+        expect(response.body._id).toBe("6915fab55fa7852213445b7c");
+
+
+    })
+
+
 });
