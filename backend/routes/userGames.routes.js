4dbe2e42c6938f41a0ac14dd386f615fc080e645
Gage Lappin <gagelappin119@gmail.com>
2025-11-17 23:02:11 -0500

    test

--- backend/routes/userGames.routes.js
+++ backend/routes/userGames.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {viewUserGames, searchUserGames, deleteUserGame, editGameInfo, addUserGame, likeGame, viewUserGameById} = require('../controllers/userGames.controller');
+const {viewUserGames, searchUserGames, deleteUserGame, editGameInfo, addUserGame, likeGame} = require('../controllers/userGames.controller');
+// commented out getting viewUserGameById from userGames controller
 const requireAuth = require('../middleware/requireAuth');
 
 router.use(requireAuth);
@@ -9,7 +10,7 @@ router.get('/', viewUserGames);
 router.post('/add', addUserGame);
 router.post('/:gameId/like', likeGame);
 router.get('/search', searchUserGames);
-router.get('/:gameId', viewUserGameById);
+// router.get('/:gameId', viewUserGameById);
 router.delete('/:gameId', deleteUserGame);
 router.patch('/:gameId', editGameInfo);
 
